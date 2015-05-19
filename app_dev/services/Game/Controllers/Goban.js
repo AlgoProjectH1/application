d16a344eef50b748aa0d78a0ba94ebc1c00213d0
Romain Quilliot <romain.addweb@gmail.com>
2015-05-19 22:28:46 +0200

    Add Goban draw

--- app_dev/services/Game/Controllers/Goban.js
+++ app_dev/services/Game/Controllers/Goban.js
@@ -1,6 +1,33 @@
 class Goban {
 
-    constructor() {
-        
+    /**
+     *
+     * Constructor
+     * @param int size
+     */
+    constructor(size, goban) {
+        this.size = size;
+        this.goban = goban;
     }
+
+    /**
+     *
+     * Draw the goban
+     */
+    draw() {
+        var toDraw = "";
+
+        for (var x = 0; x < this.size; x++) {
+            toDraw += "<tr>";
+
+            for (var y = 0; y < this.size; y++) {
+                toDraw += "<td>&nbsp;</td>";
+            }
+
+            toDraw += "</tr>";
+        }
+
+        this.goban.html(toDraw);
+    }
+
 }
