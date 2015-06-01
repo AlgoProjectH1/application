8226ee8a55bbe86bae7ebb0b1183c667d2380fdd
Romain Quilliot <romain.addweb@gmail.com>
2015-06-01 11:32:46 +0200

    Add loading anim

--- app_dev/services/app.js
+++ app_dev/services/app.js
@@ -1,7 +1,18 @@
 // Container
-Container
-    .add('Goban', new Goban(9, $('#goban')))
-    .add('Intersections', new Intersections(Container.get('Goban').getSize(), false));
+// Container
+//     .add('Goban', new Goban(9, $('#goban')))
+//     .add('Intersections', new Intersections(Container.get('Goban').getSize(), false));
 
-var gameControl = new MainControl();
-gameControl.init();
+// var gameControl = new MainControl();
+// gameControl.init();
+
+$(function () {
+    $('#login-form').on('submit', function (event) {
+        event.preventDefault();
+        $('#login-loader').addClass('active');
+
+        setTimeout(function () {
+            $('#login-loader').removeClass('active');
+        }, 3000);
+    });
+});
