d16b6d08923c2ca7adccb579c9c85b1a41839c11
Romain Quilliot <romain.addweb@gmail.com>
2015-06-01 16:49:44 +0200

    Add userApi to appJS

--- app_dev/services/app.js
+++ app_dev/services/app.js
@@ -2,6 +2,7 @@
 Container.add('HTTP', new HttpResponse(location));
 Container.add('Template', new Template('templates/'));
 Container.add('Pages', new Pages());
+Container.add('UserApi', new UserApi('http://api-swipe-login.herokuapp.com'));
 Container.get('HTTP').setURI('/login');
 
 var Routing = new Router(Container.get('HTTP'));
