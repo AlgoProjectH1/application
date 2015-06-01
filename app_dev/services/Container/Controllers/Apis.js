0e3f1aae4f86b94b81b52ddc0bcef4608210a9cb
Romain Quilliot <romain.addweb@gmail.com>
2015-06-01 17:20:36 +0200

    Update account api url

--- app_dev/services/Container/Controllers/Apis.js
+++ app_dev/services/Container/Controllers/Apis.js
@@ -1,6 +1,6 @@
 var Apis = {
     user: {
-        url: 'http://api-swipe-account.herokuapp.com',
+        url: 'http://api-account-swipe.herokuapp.com',
         key: 'P8f03uqqSL05RBMJW92wY63xx1zH98kC4f67M3bsY8cztHo9989sJ8C0xI9O7zgq'
     }
 };
