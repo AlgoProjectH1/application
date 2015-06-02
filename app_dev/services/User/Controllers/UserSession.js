a25f4e1c7a4f24b63479b2b3e5c40bbe2dad59e3
Romain Quilliot <romain.addweb@gmail.com>
2015-06-02 17:09:32 +0200

    Add userSession

---
+++ app_dev/services/User/Controllers/UserSession.js
@@ -0,0 +1,51 @@
+class UserSession {
+    
+    /**
+     * Constructor
+     * @param string token
+     */
+    constructor(token) {
+        this.token = token +':computer';
+    }
+
+    /**
+     * Verify if the user is not loggedIn
+     * @param function next
+     * @param object paramsNext
+     */
+    isNotLogged(next, paramsNext) {
+        if (!localStorage.getItem('token')) {
+            return next(paramsNext);
+        }
+
+        // Send an error
+        Container.get('HTTP').setURI('/overview');
+    }
+
+    /**
+     * Verify if the user is loggedIn
+     * @param function next
+     * @param object paramsNext
+     */
+    isLogged(next, paramsNext) {
+        var token = localStorage.getItem('token') +':computer';
+        
+        RequestErrorHandler.onError = function () {
+            localStorage.removeItem('token');
+            Container.get('HTTP').setURI('/login');
+        };
+
+        if (token != undefined) {
+            Container.get('UserApi').checkToken(token, {
+                fail: RequestErrorHandler.onError,
+                success: function (response) {
+                    next(paramsNext);
+                }
+            });
+
+            return;
+        }
+
+        RequestErrorHandler.onError();
+    }
+}
