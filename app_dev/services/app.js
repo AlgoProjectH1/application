ad93fa26854ccadb43c927069190067dc2782c60
Romain Quilliot <romain.addweb@gmail.com>
2015-06-02 17:10:23 +0200

    Update appJS

--- app_dev/services/app.js
+++ app_dev/services/app.js
@@ -1,19 +1,31 @@
 /** Init **/
-Container.add('HTTP', new HttpResponse(location));
+Container.add('HTTP', new HttpResponse());
 Container.add('Template', new Template('templates/'));
 Container.add('Pages', new Pages());
-Container.get('HTTP').setURI('/login');
 
 // APIs
 Container.add('UserApi', new UserApi(Apis.user.url, Apis.user.key));
+Container.add('UserSession', new UserSession(localStorage.getItem('token')));
 
-
+// Router
 var Routing = new Router(Container.get('HTTP'));
 var routesContainer = new RoutesContainer();
 
 
 /** Routes **/
-routesContainer.add(UserController.loginAction, {path: '/login'});
+routesContainer.add(function () {
+    Container.get('HTTP').setURI('/login');
+}, { path: '/' });
+
+routesContainer.add(UserController.loginAction, {
+    path: '/login',
+    middleware: Container.get('UserSession').isNotLogged
+});
+
+routesContainer.add(function() {console.log('lol'); }, {
+    path: '/overview',
+    middleware: Container.get('UserSession').isLogged
+});
 
 
 Routing.run(routesContainer);
