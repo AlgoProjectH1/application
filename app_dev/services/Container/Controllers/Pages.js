ddb79134c0739b8fed754ef280001c625e122a28
Romain Quilliot <romain.addweb@gmail.com>
2015-06-03 15:08:27 +0200

    Add loading to Pages

--- app_dev/services/Container/Controllers/Pages.js
+++ app_dev/services/Container/Controllers/Pages.js
@@ -1,13 +1,37 @@
 class Pages {
 
+    /**
+     * Constructor
+     */
+    constructor() {
+        this.loading = $('#loading-element');
+    }
+
+    /**
+     * Load a page
+     * @param string page
+     * @param object container
+     * @param function callback
+     */
     load(page, container, callback) {
+        this.loading.fadeIn();
+
         if (!container)
             var container = $('#content');
 
         Container.get('Template').get(page, function (template) {
             $('#content').html(template);
+            this.loading.fadeOut();
 
             callback();
-        });
+        }.bind(this));
+    }
+
+    /**
+     * Define the loading
+     * @param object loadingElement
+     */
+    setLoading(element) {
+        this.loading = element;
     }
 }
