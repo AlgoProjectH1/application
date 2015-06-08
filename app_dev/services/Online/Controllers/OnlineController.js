7ea19388fdd8b2663278f0f19eb0bdc9efe89138
Romain Quilliot <romain.addweb@gmail.com>
2015-06-08 19:05:08 +0200

    Add animation loading

--- app_dev/services/Online/Controllers/OnlineController.js
+++ app_dev/services/Online/Controllers/OnlineController.js
@@ -5,7 +5,22 @@ var OnlineController = {
      */
     lookingAction: function () {
         Container.get('Pages').load('game.looking.hbs', $('#content'), function () {
+            
             // Declenche la recherche
+            setTimeout(function () {
+                $('#player_0').removeClass('active');
+                $('#player_0').addClass('active');
+
+                setTimeout(function () {
+                    $('#player_0').removeClass('active');
+                    $('#player_1').addClass('active');
+
+                    setTimeout(function () {
+                        $('#player_1').removeClass('active');
+                        $('#player_2').addClass('active');
+                    }, 2000);
+                }, 2000);
+            }, 100);
         });
     }
 };
