ee657ec32de55ff92a5babec0dcca103c8270146
Romain Quilliot <romain.addweb@gmail.com>
2015-06-09 12:08:30 +0200

    Update looking page animation

--- app_dev/services/Online/Controllers/OnlineController.js
+++ app_dev/services/Online/Controllers/OnlineController.js
@@ -1,4 +1,6 @@
 var OnlineController = {
+    lookingAlternance: false,
+    lookingIndex: 0,
     
     /**
      * @url /looking
@@ -7,16 +9,30 @@ var OnlineController = {
         Container.get('Pages').load('game.looking.hbs', $('#content'), function () {
             
             // Declenche l'animation
-            $('.player-swipe').each(function(i) {
-                OnlineController._lookingAnimation(i);
-            });
+            OnlineController._lookingAnimation();
         });
     },
 
-    _lookingAnimation: function (nbre) {
-        setTimeout(function () {
-            $('#player_'+ (nbre - 1)).removeClass('active');
-            $('#player_'+ nbre).addClass('active');
-        }, (2000 * nbre));
+    _lookingAnimation: function () {
+        OnlineController.lookingAlternance = setInterval(function () {
+            var index = OnlineController.lookingIndex;
+            var previous = OnlineController.lookingIndex - 1;
+            var playersLength = $('.player-swipe').length;
+
+            if (index == playersLength) {
+                index = 0;
+                OnlineController.lookingIndex = 0;
+            }
+
+            $('#player_'+ previous).removeClass('active');
+            $('#player_'+ index).addClass('active');
+
+            OnlineController.lookingIndex++;
+        }, 1500);
+    },
+
+    _removeLookingAnimation: function () {
+        clearInterval(OnlineController.lookingAlternance);
+        OnlineController.lookingIndex = 0;
     }
 };
