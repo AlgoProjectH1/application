e2d3e30004ed9837f90380ee6ec6e157029035c7
Romain Quilliot <romain.addweb@gmail.com>
2015-06-19 15:44:48 +0200

    Add nav links

--- app_dev/services/User/Controllers/UserLoggedController.js
+++ app_dev/services/User/Controllers/UserLoggedController.js
@@ -1,11 +1,30 @@
-var UserLoggedController = {
+var UserLoggedController = {};
 
-    /**
-     * @url /overview
-     */
-    overviewAction: function () {
-        Container.get('Pages').load('user.overview.hbs', $('#content'), function () {
-            
-        });
-    }
+
+/**
+ * Menu page
+ * @url /overview
+ */
+UserLoggedController.overviewAction = function () {
+    Container.get('Pages').load('user.overview.hbs', $('#content'), function () {
+        // event listener
+        $('#nav-solo-choice').on('click', UserLoggedController._eventSoloChoice);
+        $('#nav-online-choice').on('click', UserLoggedController._eventOnlineChoice);
+        $('#nav-opt-choice').on('click', UserLoggedController._eventOptChoice);
+    });
+};
+
+
+/**
+ * When a user click on "Solo vs AI"
+ */
+UserLoggedController._eventSoloChoice = function () {
+
+};
+
+/**
+ * When a user click on "Multijoueur"
+ */
+UserLoggedController._eventOnlineChoice = function () {
+    Container.get('HTTP').setURI('/online');
 };
