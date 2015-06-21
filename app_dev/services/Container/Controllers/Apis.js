29dd1a44e2380d2322eb54ad7dbbdb15274a80d7
Romain Quilliot <romain.addweb@gmail.com>
2015-06-21 11:34:45 +0200

    Add matching api config

--- app_dev/services/Container/Controllers/Apis.js
+++ app_dev/services/Container/Controllers/Apis.js
@@ -2,5 +2,9 @@ var Apis = {
     user: {
         url: 'http://api-account-swipe.herokuapp.com',
         key: 'P8f03uqqSL05RBMJW92wY63xx1zH98kC4f67M3bsY8cztHo9989sJ8C0xI9O7zgq'
+    },
+
+    matching: {
+        url: 'http://api-matching-swipe.herokuapp.com/'
     }
 };
