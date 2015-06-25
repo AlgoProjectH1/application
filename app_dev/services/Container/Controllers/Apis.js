12dbb71728c6dd6c2f5257a2939dacf5e254dd78
Romain Quilliot <romain.addweb@gmail.com>
2015-06-25 23:54:13 +0200

    Update Apis conf

--- app_dev/services/Container/Controllers/Apis.js
+++ app_dev/services/Container/Controllers/Apis.js
@@ -1,11 +1,17 @@
 var Apis = {
     user: {
         url: 'http://api-account-swipe.herokuapp.com',
+        //url: 'http://localhost:1337',
         key: 'P8f03uqqSL05RBMJW92wY63xx1zH98kC4f67M3bsY8cztHo9989sJ8C0xI9O7zgq'
     },
 
     matching: {
-        url: 'http://api-matching-swipe.herokuapp.com/'
+        url: 'http://api-matching-swipe.herokuapp.com'
         //url: 'localhost:8080'
+    },
+
+    ia: {
+        url: 'http://api-game-swipe.herokuapp.com'
+        //url: 'localhost:1338'
     }
 };
